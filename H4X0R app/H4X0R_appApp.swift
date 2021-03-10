d913f34a48e60c64e91a4d45252a448ccc8770b7
Isaac <cavalcante.isaac1@gmail.com>
2021-03-10 12:32:36 -0300

    Initial Commit

---
+++ H4X0R app/H4X0R_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0R_appApp.swift
+//  H4X0R app
+//
+//  Created by Isaac Cavalcante on 10/03/21.
+//
+
+import SwiftUI
+
+@main
+struct H4X0R_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
